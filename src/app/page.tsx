f65f34aa3d5073037a552b79883a9ba743ba4d51
Jason Bui <jasonbui@Jasons-Laptop.attlocal.net>
2024-12-21 19:34:31 -0800

    added resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <a href="https://www.youtube.com/watch?v=g6gM8x9H65Q&t=71s" target="_blank" rel="noopener noreferrer">
+          <a href="https://jasonbui122124.tiiny.site/" target="_blank" rel="noopener noreferrer">
             <Image
               src="/images/profile.jpg"
               alt="Jason Bui"
@@ -81,7 +81,7 @@ export default function Home() {
               priority
             />
           </a>
-          <a href="https://www.youtube.com/watch?v=g6gM8x9H65Q&t=71s" target="_blank" rel="noopener noreferrer" className="text-gray-400 mt-2 text-xl hover:text-blue-400 transition-colors">
+          <a href="https://jasonbui122124.tiiny.site/" target="_blank" rel="noopener noreferrer" className="text-gray-400 mt-2 text-xl hover:text-blue-400 transition-colors">
             (resume)
           </a>
         </motion.div>
